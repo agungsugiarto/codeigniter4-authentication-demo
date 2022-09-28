539b8ca8411fb5a01441f1d245a4d01781235ce5
Agung Sugiarto <sugiartoagung92@gmail.com>
2022-09-28 10:32:17 +0700

    remove namespace

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,14 @@ use Config\Services;
 /** @var \CodeIgniter\Router\RouteCollection $routes */
 
 // Login user using authentication JWT.
-$routes->group('api/v1', ['namespace' => 'App\Controllers\JWTAuth'], function (Routes $routes) {
+$routes->group('api/v1', function (Routes $routes) {
     $routes->group('basic', ['filter' => 'auth.basic:web,email,onceBasic'], function ($routes) {
         $routes->get('user', function () {
             return Services::response()->setJSON(auth()->user());
         });
     });
-    $routes->post('login', 'JWTAuthController::login');
-    $routes->post('logout', 'JWTAuthController::logout', ['filter' => 'auth:api']);
-    $routes->post('refresh', 'JWTAuthController::refresh', ['filter' => 'auth:api']);
-    $routes->match(['get', 'post'], 'user', 'JWTAuthController::user', ['filter' => 'auth:api']);
+    $routes->post('login', '\App\Controllers\JWTAuth\JWTAuthController::login');
+    $routes->post('logout', '\App\Controllers\JWTAuth\JWTAuthController::logout', ['filter' => 'auth:api']);
+    $routes->post('refresh', '\App\Controllers\JWTAuth\JWTAuthController::refresh', ['filter' => 'auth:api']);
+    $routes->match(['get', 'post'], 'user', '\App\Controllers\JWTAuth\JWTAuthController::user', ['filter' => 'auth:api']);
 });
